ceb249a4fa55cf72e8e1df1be68b2fd789c604c2
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-05-10 12:29:28 -0300

    declarando variavel

--- EstudosJava/primeiroprojeto/src/Main.java
+++ EstudosJava/primeiroprojeto/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Esté é o Stream Mach");
+        System.out.println("Filme: Top Gun Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("O Ano de lançamento do filme é: " + anoDeLancamento);
+
+        boolean incluiNoPlano= true;
+        double notaFilme = 8.1;
+
     }
 }
