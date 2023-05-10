ec4c0d117dfb538a393eabab950521205ca9aee1
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-05-10 20:59:13 -0300

    Exemplo de casting

--- EstudosJava/primeiroprojeto/src/Main.java
+++ EstudosJava/primeiroprojeto/src/Main.java
@@ -6,8 +6,23 @@ public class Main {
         int anoDeLancamento = 2022;
         System.out.println("O Ano de lançamento do filme é: " + anoDeLancamento);
 
-        boolean incluiNoPlano= true;
         double notaFilme = 8.1;
+        double media = (notaFilme + 9.1 + 6.1) /3 ;
+        System.out.println("A Média do filme é: " + media);
+
+        //Parte do código para sinopse!
+
+        String sinopse;
+        sinopse = """
+                Top Gun Maverick:
+                Filme muito bom, com galã dos anos 80
+                ano de lançamento
+                """ + anoDeLancamento;
+
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media / 2);
+        System.out.println("Média de estrelas é: " + classificacao);
 
     }
 }
