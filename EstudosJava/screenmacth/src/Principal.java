e7c1ba91698297007781dca8fa0d297794f73b7c
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-05-13 19:38:14 -0300

    metodo Get e Set

--- EstudosJava/screenmacth/src/Principal.java
+++ EstudosJava/screenmacth/src/Principal.java
@@ -1,10 +1,11 @@
+import br.com.alura.streammacth.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme filme1 = new Filme();
-
-        filme1.nome = "Milagre da cela 5";
-        filme1.anoDeLancamento = 2018;
-        filme1.duracaoEmMinutos = 185;
+        filme1.setNome("Milagre da cela 5");
+        filme1.setAnoDeLancamento(2018);
+        filme1.setDuracaoEmMinutos(185);
 
         filme1.exibeFilme();
 
@@ -13,6 +14,7 @@ public class Principal {
         filme1.somaDasAvaliacoes(6);
 
         System.out.println("A nota do filme é: " + filme1.calcularMedia());
+        System.out.println("br.com.alura.streammacth.modelos.Filme Avaliado por: " + filme1.getTotalDeAvaliacoes() + " usuários");
 
     }
 }
