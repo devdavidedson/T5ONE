4ae4be0765de643d1452d40135e03c3ce2319632
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-05-13 10:47:30 -0300

    Desafio

--- EstudosJava/BankAccount/src/Desafio.java
+++ EstudosJava/BankAccount/src/Desafio.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class DesafioContaBanco {
+public class Desafio {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int saldo = 2500;
+        double saldo = 2500;
         int acessoMenu = 0;
         String menu = """
                 *********************************************
@@ -15,20 +15,19 @@ public class DesafioContaBanco {
                 Saldo inicial:           R$: 2500
                 
                 *********************************************
+                
                 """;
         String subMenu = """
                 1- Consultar Saldo.
                 2- Receber Depósito.
                 3- transferir.
                 4- para sair.
+                
                 """;
 
         System.out.println(menu);
 
         while (acessoMenu != 4) {
-
-            int recebePix = 0;
-            int transferencia =0;
 
             System.out.println(subMenu);
             System.out.println("Digite uma opção do Menu");
@@ -38,26 +37,25 @@ public class DesafioContaBanco {
                 System.out.println("Seu saldo é: " + saldo);
             } else if (acessoMenu == 2) {
                 System.out.println("Qual valor do depósito?");
-                recebePix = scanner.nextInt();
-                saldo = saldo + recebePix;
+                double recebePix = scanner.nextInt();
+                saldo += recebePix;
                 System.out.println("Você recebeu um depósito, seu novo saldo é: " + saldo);
             } else if (acessoMenu == 3) {
                 System.out.println("Quanto você deseja retirar:");
-                transferencia = scanner.nextInt();
+                double transferencia = scanner.nextInt();
                 if (transferencia > saldo) {
                     System.out.println("Valor de retirada maior que o Saldo em conta");
                 } else {
-                    saldo = saldo - transferencia;
+                    saldo -= transferencia;
                     System.out.println("Você fez uma transferência, seu novo saldo é: " + saldo);
                 }
-            } else if (acessoMenu > 4) {
+            } else if (acessoMenu != 4) {
                 System.out.println("Operação inválida, tente novamente");
             } else {
-                System.out.println("Encerrada a sessão.");
+                System.out.println("Sessão encerada.");
                 break;
             }
         }
-
 
     }
 }
