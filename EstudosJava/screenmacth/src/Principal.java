6594c49d6dbe0c4e576f60b5460c778ac182a9a6
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-05-14 01:48:20 -0300

    heranca

--- EstudosJava/screenmacth/src/Principal.java
+++ EstudosJava/screenmacth/src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.alura.streammacth.modelos.Filme;
+import br.com.alura.streammacth.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -6,15 +7,27 @@ public class Principal {
         filme1.setNome("Milagre da cela 5");
         filme1.setAnoDeLancamento(2018);
         filme1.setDuracaoEmMinutos(185);
+        filme1.setIncluidoNoPlano(true);
 
         filme1.exibeFilme();
-
         filme1.somaDasAvaliacoes(8);
         filme1.somaDasAvaliacoes(10);
         filme1.somaDasAvaliacoes(6);
 
         System.out.println("A nota do filme é: " + filme1.calcularMedia());
-        System.out.println("br.com.alura.streammacth.modelos.Filme Avaliado por: " + filme1.getTotalDeAvaliacoes() + " usuários");
+        System.out.println("Filme Avaliado por: " + filme1.getTotalDeAvaliacoes() + " usuários");
+
+        Serie heros = new Serie();
+        heros.setNome("Heroes");
+        heros.setAnoDeLancamento(2006);
+        heros.setDuracaoEmMinutos(300);
+        heros.setTemporadas(6);
+        heros.setIncluidoNoPlano(true);
+        heros.setEpisodiosPorTemporada(8);
+        heros.setMinutosPorEpisodio(44);
+        heros.exibeFilme();
+
+        System.out.println("Para maratorna leva: " + heros.getDuracaoEmMinutos() + " min");
 
     }
 }
---
+++ EstudosJava/screenmacth/src/br/com/alura/streammacth/modelos/Titulo.java
@@ -0,0 +1,62 @@
+package br.com.alura.streammacth.modelos;
+
+public class Titulo {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void exibeFilme() {
+        System.out.println("Filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        System.out.println("Duração: " + duracaoEmMinutos + " min");
+        System.out.println("Incluido no Plano: " + incluidoNoPlano);
+    }
+
+    public void somaDasAvaliacoes(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double calcularMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+}
