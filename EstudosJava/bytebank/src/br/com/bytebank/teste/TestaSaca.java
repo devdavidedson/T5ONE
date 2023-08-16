adaaa5adff535356ab7c02a3605524b3d6aa95fd
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-08-16 20:33:48 -0300

    criando visibilidade protected

--- EstudosJava/bytebank/src/br/com/bytebank/teste/TestaSaca.java
+++ EstudosJava/bytebank/src/br/com/bytebank/teste/TestaSaca.java
@@ -1,5 +1,6 @@
 package br.com.bytebank.teste;
 
+import br.com.bytebank.especial.ContaEspecial;
 import br.com.bytebank.modelo.*;
 
 public class TestaSaca {
@@ -7,15 +8,19 @@ public class TestaSaca {
     public static void main(String[] args) {
 
         Conta conta = new ContaCorrente(123, 321);
+        ContaEspecial ce = new ContaEspecial(122, 323123);
+
+        ce.deposita(400);
+        System.out.println(ce.getSaldo());
 
         conta.deposita(250);
         try {
             conta.saca(260);
         } catch (SaldoInsuficienteException ex) {
-            System.out.println("Ex: " + ex.getMessage());
+            System.out.println("Saldo insuficiente: " + ex.getMessage());
         }
 
-        System.out.println(conta.getSaldo());
+        System.out.println("Saldo atual: " + conta.getSaldo());
     }
 
 }
