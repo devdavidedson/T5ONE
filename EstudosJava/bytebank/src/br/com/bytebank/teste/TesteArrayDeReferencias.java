8e50ce27c925fa45acdde38fcaadde56b3e69956
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-08-17 09:38:15 -0300

    Criando Array de referencias

---
+++ EstudosJava/bytebank/src/br/com/bytebank/teste/TesteArrayDeReferencias.java
@@ -0,0 +1,22 @@
+package br.com.bytebank.teste;
+
+import br.com.bytebank.modelo.ContaCorrente;
+
+public class TesteArrayDeReferencias {
+    public static void main(String[] args) {
+
+        ContaCorrente[] contas = new ContaCorrente[5];
+        ContaCorrente cc1 = new ContaCorrente(324, 54523);
+        contas[0] = cc1;
+
+        ContaCorrente cc2 = new ContaCorrente(644, 53223);
+        contas[1] = cc2;
+
+        ContaCorrente ref = contas[1];
+
+        System.out.println(contas[0].getAgencia());
+        System.out.println(cc1.getAgencia());
+        System.out.println(ref.getAgencia());
+
+    }
+}
